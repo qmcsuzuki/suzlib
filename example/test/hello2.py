f58a9d22aaf14ea5d40ce63eb807371abcfcb7c6
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2023-08-06 17:53:45 +0900

    Create hello2.py

---
+++ example/test/hello2.py
@@ -0,0 +1,8 @@
+# verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A
+import example.Helloworld
+
+def main():
+    print(example.Helloworld.get_hello_world())
+
+if __name__ == '__main__':
+    main()
