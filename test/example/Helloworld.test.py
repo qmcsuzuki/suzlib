9e6954bbf848fab537e6f98226a7b7012c8adcc5
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2023-08-06 16:57:32 +0900

    Update Helloworld.test.py

--- test/example/Helloworld.test.py
+++ test/example/Helloworld.test.py
@@ -1,8 +1,5 @@
-# verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A
-import test.example.Helloworld
-
 def main():
-    print(test.example.Helloworld.get_hello_world())
+    print("Yes")
 
 if __name__ == '__main__':
     main()
