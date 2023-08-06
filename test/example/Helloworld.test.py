d4f97a046a2e73b08ba35ae80d32c3343d109675
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2023-08-06 17:18:25 +0900

    Delete Helloworld.test.py

--- test/example/Helloworld.test.py
+++
@@ -1,8 +0,0 @@
-# verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A
-import test.example.Helloworld
-
-def main():
-    print(test.example.Helloworld.get_hello_world())
-
-if __name__ == '__main__':
-    main()
