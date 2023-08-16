0f7aeb7d08af34b62a641e8a4ec7f987c3f60def
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2023-08-16 17:11:18 +0900

    Create DeletableHeapq.py

---
+++ data_structure/DeletableHeapq.py
@@ -0,0 +1,34 @@
+from heapq import *
+class deletable_heapq:
+    """
+    削除可能heapq
+    削除する元があることを仮定している（必要なときは別途 set や dict を持つ）
+    """
+    def __init__(self, initial = []):
+        if initial:
+            self.q = initial
+            heapify(self.q)
+        else: self.q = []
+        self.q_del = []
+
+    def propagate(self):
+        while self.q_del and self.q[0] == self.q_del[0]:
+            heappop(self.q)
+            heappop(self.q_del)
+
+    def heappop(self):
+        self.propagate()
+        return heappop(self.q)
+    
+    def __len__(self):
+        return len(self.q) - len(self.q_del)        
+
+    def top(self):
+        self.propagate()
+        return self.q[0]
+            
+    def remove(self,x):
+        heappush(self.q_del,x)
+
+    def heappush(self,x):
+        heappush(self.q,x)
