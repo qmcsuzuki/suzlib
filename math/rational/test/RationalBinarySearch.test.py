7b5c853a81a99dbea90efd3a53bc9b70fb7112f8
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2023-10-22 01:12:43 +0900

    Update RationalBinarySearch.test.py

--- math/rational/test/RationalBinarySearch.test.py
+++ math/rational/test/RationalBinarySearch.test.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import sys
 sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
 
-from rational import RationalBinarySearch
+from rational.RationalBinarySearch import RationalBinarySearch
 
 def main():
     readline = sys.stdin.readline
