09190b5030e5861cc9c705901657c41f05a5ff41
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2023-10-22 00:40:21 +0900

    Update RationalBinarySearch.test.py

--- math/rational/test/RationalBinarySearch.test.py
+++ math/rational/test/RationalBinarySearch.test.py
@@ -1,5 +1,5 @@
 # verification-helper: PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1208
-from math.rational.RationalBinarySearch import *
+from .. import RationalBinarySearch
 
 import sys
 readline = sys.stdin.readline
