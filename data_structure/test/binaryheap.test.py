9804562400a8d345e2e0e85a40b84406607a6e65
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2023-10-22 01:26:34 +0900

    Update binaryheap.test.py

--- data_structure/test/binaryheap.test.py
+++ data_structure/test/binaryheap.test.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 import sys
 sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
-from data_structure.BibaryHeap import BinaryHeap
+from data_structure.BinaryHeap import BinaryHeap
 readline = sys.stdin.readline
 
 def main():
